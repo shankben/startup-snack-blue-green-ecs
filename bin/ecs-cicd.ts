356954119dd8294ffd2f5bac194f9bce5ee55380
Ben Shank <benshank@amazon.com>
2020-10-15 12:38:23 -0500

    Initial commit

---
+++ bin/ecs-cicd.ts
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from '@aws-cdk/core';
+import { EcsCicdStack } from '../lib/ecs-cicd-stack';
+
+const app = new cdk.App();
+new EcsCicdStack(app, 'EcsCicdStack');
